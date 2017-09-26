99408c81ad2d502b27c8506358490e1530c99866
yadlapalli.deepak <yadlapalli.deepak@CSELAB02-14.bennett.edu.in>
2017-09-26 11:46:17 +0530

    code

--- src/junittestingPackage/testAddStrings.java
+++ src/junittestingPackage/testAddStrings.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 public class testAddStrings {
 
 	
+
 	@Test
 	public void test() {
 		junitFunction junitstring = new junitFunction();
--- src/junittestingPackage/testAddnumbers.java
+++ src/junittestingPackage/testAddnumbers.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class testAddnumbers {
 
 	
-	@Test
+
 	public void test() {
 		junitFunction junit = new junitFunction();
 		int result = junit.addNumbers(100, 200);
